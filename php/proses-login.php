580371f87acb77a5aa6fe375fef8e2d6b38d430a
refido <revidoberliano@gmail.com>
2020-07-01 22:04:13 +0700

    admin proses db

---
+++ php/proses-login.php
@@ -0,0 +1,21 @@
+<?php
+include "koneksi.php";
+
+$email = $_POST['email'];
+$password = md5($_POST['password']);
+$error = "username / password salah!";
+
+$sql = "select * from user where email='$email'and password='$password'";
+$result = mysqli_query($connect, $sql);
+$cek = mysqli_num_rows($result);
+
+if ($cek > 0) {
+    session_start();
+    $_SESSION['email'] = $email;
+    $_SESSION['status'] = 'login';
+    header("location:http://localhost/online_shop/index-admin.html?login=berhasil");
+} else {
+    $_SESSION['error'] = $error;
+    header("location:http://localhost/online_shop/login.html?login=gagal");
+    echo mysqli_error($connect);
+}
